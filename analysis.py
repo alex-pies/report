8c797ea5d9cc9c12c1d1c304d89e8b4aed8518f8
chengxiongchao <1223408988@qq.com>
2019-11-01 14:45:15 +0800

    ADD: general_subject_credit_warnings

--- analysis.py
+++ analysis.py
@@ -109,15 +109,39 @@ def general_subject_analysis():
     rates_df.plot.line().get_figure().savefig(os.path.join('images', '通识选修所有学生修得学分占比折线图'+'.png'))
 
 
-def general_subject_credit_warnings(single_stu):
+def general_subject_credit_warnings(grade, no, std_credits):
     """
     通识选修课没够的人
     :return:  列表，列表元素是 没够的人 + 不够的不同学科的学分
     """
-    pass
+    file = grade + '_result.csv'
+    if not os.path.isfile(file):
+        print('no such file')
+        return
+    df = pd.read_csv(file, index_col='学号')
+    df.课程性质, df.课程名称, df.学分 = df.课程性质.astype(str), df.课程名称.astype(str), df.学分.astype('float64')
+    df = df.loc[no]
+    if df.shape[0] == 0:
+        print('no such student.')
+        return
+    df['课程性质'] = df.apply(lambda x: x.课程性质[5:].replace('(', '').replace(')', '') if '通识' in x.课程性质 else 'nan', axis=1)
+    df = df[df.课程性质 != 'nan']
+    print(df[['课程性质', '课程名称', '学分']])
+    dff = df[['课程性质', '课程名称', '学分']]
+    print(dff.groupby('课程性质').agg({'学分': 'sum'}).sort_index())
+    student_result = dff.groupby('课程性质').agg({'学分': 'sum'})
+
+    std_df = pd.DataFrame(data={'标准': [item[1] for item in std_credits]}, index=[item[0] for item in std_credits])
+    print('std: ', std_df)
+    print(std_df.join(student_result))
+
+    real_result = std_df.join(student_result)
+    sub = (real_result.标准 - real_result.学分)
+    print(sub[sub > 0])
 
 
 if __name__ == '__main__':
     clean_data()
     # single_subject_compare('大学英语Ⅰ')
     # general_subject_analysis()
+    general_subject_credit_warnings('2014', 201414010426, [('人文', 19.0), ('自然', 20.0), ('艺体', 21.0)])
